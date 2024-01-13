d52206183ed33283cb6fb947cc0b0b957de41e39
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-01-13 12:38:21 +0000

    update port settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 8080,
+	},
+
 	plugins: [sveltekit()],
 
 	build: {
